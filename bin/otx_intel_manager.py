80863f17ba1df83b89f599cbea46edb5a78fb9b6
Luke Monahan <luke.monahan@rivium.com.au>
2018-10-31 11:40:11 +1100

    Updated to use modern modinput framwork, some other enhancements

--- bin/otx_intel_manager.py
+++ bin/otx_intel_manager.py
@@ -1,42 +1,154 @@
 import sys
+import os
+import hashlib
 import time
 import json
+import urllib
 from collections import defaultdict
 
-from modular_input import ModularInput, DurationField, IntegerField, Field
-
+from splunklib.modularinput import *
+from splunklib.six.moves.urllib.parse import urlsplit
 import splunklib.client as client
 import splunklib.results as results
 
-class OTXIntelManagerModularInput(ModularInput):
+class OTXIntelManagerModularInput(Script):
 
-	def __init__(self):
+	def get_scheme(self):
+		scheme = Scheme("Open Threat Exchange Intel Manager")
+		scheme.description = "Integrate OTX pulses into the Splunk ES Threat Intelligence framework"
+		scheme.use_external_validation = False
+		scheme.use_single_instance = False
 
-		scheme_args = {'title': "Open Threat Exchange Intel Manager",
-					   'description': "Integrate OTX pulses into the Splunk ES Threat Intelligence framework",
-					   'use_external_validation': "true",
-					   'streaming_mode': "xml",
-					   'use_single_instance': "true"}
+		otx_index = Argument("otx_index")
+		otx_index.data_type = Argument.data_type_string
+		otx_index.title = "OTX index"
+		otx_index.description = "The index in which we will look to find the OTX events"
+		otx_index.required_on_create = True
+		otx_index.required_on_edit = True
+		scheme.add_argument(otx_index)
 
-		args = [
-				Field("otx_index", "OTX index", "The index in which we will find the OTX events", empty_allowed=False),
-				IntegerField("backfill_days", "Backfill days", "The number of days to backfill indicators for on first run", empty_allowed=False),
-				DurationField("interval", "Interval", "The interval defining how often to check for new indicators; can include time units (e.g. 15m for 15 minutes, 8h for 8 hours)", empty_allowed=False)
-		]
+		backfill_days = Argument("backfill_days")
+		backfill_days.data_type = Argument.data_type_number
+		backfill_days.title = "Backfill days"
+		backfill_days.description = "The number of days to backfill indicators for on first run"
+		backfill_days.required_on_create = True
+		backfill_days.required_on_edit = True
+		scheme.add_argument(backfill_days)
 
-		ModularInput.__init__( self, scheme_args, args, logger_name='otx_intel_manager' )
+		return scheme
 
-	def run(self, stanza, cleaned_params, input_config):
+	def stream_events(self, inputs, ew):
 
-		interval = cleaned_params["interval"]
-		backfill_days = cleaned_params["backfill_days"]
-		index = cleaned_params.get("otx_index", "otx")
-		source = stanza
 		BATCH_SIZE = 50
 
-        	run_time = time.time()
+		for input_name, input_item in inputs.inputs.iteritems():
 
-		type_mappings = {
+			otx_index = str(input_item["otx_index"])
+			backfill_days = int(input_item["backfill_days"])
+			run_time = time.time()
+
+			ew.log(ew.INFO, "Beginning scan of index %s for OTX threat intel" % otx_index)
+
+			try:
+				checkpoint_data = self.get_checkpoint_data(inputs.metadata["checkpoint_dir"], input_name)
+			except IOError:
+				checkpoint_data = None
+
+			# Try to load the last ran date from the checkpoint data
+			if checkpoint_data is not None and 'last_ran' in checkpoint_data:
+				last_ran = checkpoint_data['last_ran']
+			else:
+				last_ran = None
+
+		 	if last_ran is not None:
+		 		et = str(last_ran)
+		 	else:
+		 		et = "-%dd" % backfill_days
+
+			# Get a service handle back to splunkd
+			splunkd_uri = self._input_definition.metadata["server_uri"]
+			session_key = self._input_definition.metadata["session_key"]
+			splunkd = urlsplit(splunkd_uri, allow_fragments=False)
+			service = client.Service(
+			    scheme=splunkd.scheme,
+			    host=splunkd.hostname,
+			    port=splunkd.port,
+			    token=session_key,
+				owner="nobody"
+			)
+
+			indicator_query = "search index=%s earliest=%s latest=%d sourcetype=otx:indicator | fields _time type indicator pulse_id" % (otx_index, et, run_time)
+
+			ew.log(ew.INFO, "Fetching indicators...")
+
+			indicator_results = service.jobs.oneshot(indicator_query, count=0)
+			indicator_reader = results.ResultsReader(indicator_results)
+
+			indicator_batches = defaultdict(list)
+
+			indicator_count_total = 0
+			indicator_count_mapped = 0
+			type_mappings = self.get_type_mappings()
+
+			for indicator in indicator_reader:
+				indicator_count_total = indicator_count_total + 1
+				if indicator['type'] in type_mappings:
+					for mapping in type_mappings[indicator['type']]:
+						indicator_batches[mapping['intel_collection']].append({
+							"time": indicator['_time'],
+							mapping['intel_field']: indicator['indicator'],
+							"threat_key": "otx:%s" % indicator['pulse_id']
+						})
+						indicator_count_mapped = indicator_count_mapped + 1
+
+			ew.log(ew.INFO, "Found %d indicators, which will result in %d new threat intel entries" % (indicator_count_total, indicator_count_mapped))
+			ew.log(ew.INFO, "Beginning push of indicators into threat collections")
+
+			for threat_collection in indicator_batches:
+				threat_items = indicator_batches[threat_collection]
+				ew.log(ew.INFO, "Pushing %d items into %s (batch size %d)" % (len(threat_items), threat_collection, BATCH_SIZE))
+				for small_batch in ([threat_items[i:i+BATCH_SIZE] for i in range(0, len(threat_items), BATCH_SIZE)]):
+					body = { "item" : json.dumps(small_batch) }
+					resp = service.post('/services/data/threat_intel/item/%s' % (threat_collection), body=urllib.urlencode(body))
+					if resp['status'] >= 400:
+						ew.log(ew.ERROR, "Error when writing a batch of indicators: %s" % str(resp))
+
+			pulse_query = 'search index=%s earliest=%s latest=%d sourcetype=otx:pulse | dedup id | eval threat_category=mvjoin(mvdedup(mvappend($industries{}$, $tags{}$, $targeted_countries{}$)), "|") | fillnull value="" adversary description threat_category | fields _time adversary id name description threat_category' % (otx_index, et, run_time)
+
+			ew.log(ew.INFO, "Fetching pulses")
+
+			pulse_results = service.jobs.oneshot(pulse_query, count=0)
+			pulse_reader = results.ResultsReader(pulse_results)
+
+			threat_group_intel = service.kvstore["threat_group_intel"]
+
+			threat_group_items = []
+			for pulse in pulse_reader:
+				threat_group_item = {
+					"_key" : "otx:%s" % pulse['id'],
+					"time" : pulse['_time'],
+					"description" : "%s: %s" % (pulse['name'], pulse['description']),
+					"source_type" : "otx",
+					"source_id" : pulse['id'],
+					"source_path" : "https://otx.alienvault.com/pulse/%s" % pulse['id'],
+					"source_digest" : pulse['id'],
+					"threat_group" : pulse['adversary'],
+					"threat_category" : pulse['threat_category'].split("|")
+				}
+				threat_group_items.append(threat_group_item)
+
+			ew.log(ew.INFO, "Found %d new or updated pulses to load into threat intel collections" % len(threat_group_items))
+			ew.log(ew.INFO, "Beginning push of pulses into threat collections")
+
+			for small_batch in ([threat_group_items[i:i+BATCH_SIZE] for i in range(0, len(threat_group_items), BATCH_SIZE)]):
+				threat_group_intel.data.batch_save(*small_batch)
+
+			self.save_checkpoint_data(inputs.metadata["checkpoint_dir"], input_name,  { 'last_ran': run_time })
+
+			ew.log(ew.INFO, "Done OTX threat intel management")
+
+	def get_type_mappings(self):
+		return {
 			"domain": [
 				{ "intel_collection": "ip_intel", "intel_field": "domain" },
 				{ "intel_collection": "http_intel", "intel_field": "domain" },
@@ -102,85 +214,30 @@ class OTXIntelManagerModularInput(ModularInput):
 			]
 		}
 
-		if self.needs_another_run(input_config.checkpoint_dir, stanza, interval):
+	def get_checkpoint_data(self, checkpoint_dir, stanza="(undefined)"):
+	    fp = None
 
-			try:
-				checkpoint_data = self.get_checkpoint_data(input_config.checkpoint_dir, stanza, throw_errors=True)
-			except IOError:
-				checkpoint_data = None
-			except ValueError:
-				self.logger.exception("Exception generated when attempting to load the checkpoint data")
-				checkpoint_data = None
+	    try:
+	        fp = open(self.get_file_path(checkpoint_dir, stanza) )
+	        checkpoint_dict = json.load(fp)
+	        return checkpoint_dict
+	    finally:
+	        if fp is not None:
+	            fp.close()
 
-			# Try to load the last ran date from the checkpoint data
-			if checkpoint_data is not None and 'last_ran' in checkpoint_data:
-				last_ran = checkpoint_data['last_ran']
-			else:
-				last_ran = None
+	def save_checkpoint_data(self, checkpoint_dir, stanza, data):
+	    fp = None
 
-		 	if last_ran is not None:
-		 		et = str(last_ran)
-		 	else:
-		 		et = "-%dd" % backfill_days
+	    try:
+	        fp = open(self.get_file_path(checkpoint_dir, stanza), 'w' )
+	        json.dump(data, fp)
+	    finally:
+	        if fp is not None:
+	            fp.close()
 
-			service = client.Service(token=input_config.session_key, owner="nobody")
-
-			indicator_query = "search index=%s earliest=%s latest=%d sourcetype=otx:indicator | fields _time type indicator pulse_id" % (index, et, run_time)
-
-			indicator_results = service.jobs.oneshot(indicator_query, count=0)
-			indicator_reader = results.ResultsReader(indicator_results)
-
-			indicator_batches = defaultdict(list)
-
-			for indicator in indicator_reader:
-				if indicator['type'] in type_mappings:
-					for mapping in type_mappings[indicator['type']]:
-						indicator_batches[mapping['intel_collection']].append({
-							"time": indicator['_time'],
-							mapping['intel_field']: indicator['indicator'],
-							"threat_key": "otx:%s" % indicator['pulse_id']
-						})
-
-			for threat_collection in indicator_batches:
-				threat_items = indicator_batches[threat_collection]
-				for small_batch in ([threat_items[i:i+BATCH_SIZE] for i in range(0, len(threat_items), BATCH_SIZE)]):
-					service.post('/services/data/threat_intel/item/%s' % (threat_collection), body='item=%s' % (json.dumps(small_batch)))
-
-			pulse_query = 'search index=%s earliest=%s latest=%d sourcetype=otx:pulse | eval threat_category=mvjoin($tags{}$, "|") | fillnull value="" adversary description threat_category | fields _time adversary id name description threat_category' % (index, et, run_time)
-			pulse_results = service.jobs.oneshot(pulse_query, count=0)
-			pulse_reader = results.ResultsReader(pulse_results)
-
-			threat_group_intel = service.kvstore["threat_group_intel"]
-
-			threat_group_items = []
-			for pulse in pulse_reader:
-				threat_group_item = {
-					"_key" : "otx:%s" % pulse['id'],
-					"time" : pulse['_time'],
-					"description" : "%s: %s" % (pulse['name'], pulse['description']),
-					"source_type" : "otx",
-					"source_id" : pulse['id'],
-					"source_path" : "https://otx.alienvault.com/pulse/%s" % pulse['id'],
-					"source_digest" : pulse['id'],
-					"threat_group" : pulse['adversary'],
-					"threat_category" : pulse['threat_category'].split("|")
-				}
-				threat_group_items.append(threat_group_item)
-			for small_batch in ([threat_group_items[i:i+BATCH_SIZE] for i in range(0, len(threat_group_items), BATCH_SIZE)]):
-				threat_group_intel.data.batch_save(*small_batch)
-
-			self.save_checkpoint_data(input_config.checkpoint_dir, stanza,  { 'last_ran': run_time })
+	def get_file_path(self, checkpoint_dir, stanza):
+		return os.path.join( checkpoint_dir, hashlib.sha224(stanza).hexdigest() + ".json" )
 
 
-if __name__ == '__main__':
-	try:
-		otx_intel_manager_input = OTXIntelManagerModularInput()
-		otx_intel_manager_input.execute()
-		sys.exit(0)
-	except Exception as exception:
-
-		# This logs general exceptions that would have been unhandled otherwise (such as coding errors)
-		if otx_intel_manager_input is not None:
-			otx_intel_manager_input.logger.exception("Unhandled exception was caught, this may be due to a defect in the script")
-
-		raise exception
+if __name__ == "__main__":
+	sys.exit(OTXIntelManagerModularInput().run(sys.argv))
